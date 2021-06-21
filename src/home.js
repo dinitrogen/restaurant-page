1b51f22c7ab721c6d27fa6e8030ae16d3493bf82
dinitrogen <noshowdon@gmail.com>
2021-06-21 17:01:53 -0400

    Setup basic skeleton of the project.

---
+++ src/home.js
@@ -0,0 +1,6 @@
+const homePage = function() {
+    const contentDiv = document.querySelector('#content');
+    contentDiv.textContent = "Welcome to my restaurant!";
+}
+
+export { homePage }
---
+++ src/menu.js
@@ -0,0 +1,6 @@
+const menuPage = function() {
+    const contentDiv = document.querySelector('#content');
+    contentDiv.textContent = "Here is the menu!";
+}
+
+export { menuPage }
